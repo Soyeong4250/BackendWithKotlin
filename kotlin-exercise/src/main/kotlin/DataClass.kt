ab4c03e49b93203e22e55b89299886b9b74ad083
이소영 <th42500@naver.com>
2023-08-20 22:13:15 +0900

    [Feat] 데이터 클래스 - equals(), hashCode() 학습완료

---
+++ kotlin-exercise/src/main/kotlin/DataClass.kt
@@ -0,0 +1,29 @@
+class Person (val name: String, val age: Int) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Person
+
+        if (name != other.name) return false
+        if (age != other.age) return false
+
+        return true
+    }
+
+//    override fun hashCode(): Int {
+//        var result = name.hashCode()
+//        result = 31 * result + age
+//        return result
+//    }
+}
+
+fun main() {
+    val person1 = Person(name = "tony", age = 12)
+    val person2 = Person(name = "tony", age = 12)
+
+    println(person1 == person2)  // 일반 class일 경우 false, data class일 경우 true
+
+    val hashSet = hashSetOf(person1)
+    println(hashSet.contains(person2))  // hashCode가 주석일 경우에는 false, 주석이 아닐 경우에는 true
+}
