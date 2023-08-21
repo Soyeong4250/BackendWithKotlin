3670cdf695ee2c491f9eac939e40dc35b79ba448
Soyeong4250 <th42500@naver.com>
2023-08-21 14:44:51 +0900

    [Feat] 데이터 클래스 toString() 학습 완료

--- kotlin-exercise/src/main/kotlin/DataClass.kt
+++ kotlin-exercise/src/main/kotlin/DataClass.kt
@@ -1,15 +1,15 @@
-class Person (val name: String, val age: Int) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Person
-
-        if (name != other.name) return false
-        if (age != other.age) return false
-
-        return true
-    }
+data class Person (val name: String, val age: Int) {
+//    override fun equals(other: Any?): Boolean {
+//        if (this === other) return true
+//        if (javaClass != other?.javaClass) return false
+//
+//        other as Person
+//
+//        if (name != other.name) return false
+//        if (age != other.age) return false
+//
+//        return true
+//    }
 
 //    override fun hashCode(): Int {
 //        var result = name.hashCode()
@@ -26,4 +26,7 @@ fun main() {
 
     val hashSet = hashSetOf(person1)
     println(hashSet.contains(person2))  // hashCode가 주석일 경우에는 false, 주석이 아닐 경우에는 true
+
+    println(person1.toString())  // 일반 class일 경우 Person@5451c3a8, data class일 경우 Person(name=tony, age=12)
+
 }
