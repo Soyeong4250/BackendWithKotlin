b89c0a28d6585cfe9fffaa9b86c81dcef77710d7
이소영 <th42500@naver.com>
2024-12-03 18:15:17 +0900

    [Feat] 함수 학습 진행중

---
+++ kotlin-exercise/src/main/kotlin/2_Function.kt
@@ -0,0 +1,31 @@
+// 기본적인 함수 선언 스타일
+fun sum(a: Int, b: Int) : Int {
+    return a + b
+}
+
+// 표현식 스타일
+fun sum2(a: Int, b: Int): Int = a + b
+
+// 표현식 & 반환타입 생략
+// 함수도 변수와 마찬가지로 반환 타입을 컴파일러가 기본적으로 추론해주기 때문에 표현식 스타일에서 반환 타입을 생략하여도 컴파일 오류가 발생하지 않음
+fun sum3(a: Int, b: Int) = a + b
+
+// 몸통이 있는 함수는 반환 타입 생략 시 컴파일 오류 발생
+/*fun sum4(a: Int, b: Int) {
+    return a + b
+}*/
+
+// 반환타입이 없는 함수는 Unit을 반환
+fun printSum(a:Int, b: Int): Unit {
+    println("$a + $b = ${a + b}")
+}
+
+// 디폴트 파라미터
+fun greeting(message: String = "안녕하세요!") {
+    println(message)
+}
+
+fun main() {
+    greeting()
+    greeting("Hi!!!!")
+}
