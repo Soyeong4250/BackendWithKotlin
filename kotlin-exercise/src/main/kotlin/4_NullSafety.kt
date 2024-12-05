b29223842a9d404d242490d0103d300cafbf4ee2
이소영 <th42500@naver.com>
2024-12-05 18:47:45 +0900

    [Feat] 널 안정성 학습 진행중

---
+++ kotlin-exercise/src/main/kotlin/4_NullSafety.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    /*val a : String = null
+    var b : String = "aabbcc"
+    b = null*/
+
+
+    var a : String? = null
+//    println(a.length)
+    println(a?.length)
+
+
+    val b: Int = if (a != null) a.length else 0
+    println(b)
+
+    // 엘비스 연산자
+    val c = a?.length ?: 0
+    println(c)
+
+}
